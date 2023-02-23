8291ac5b9794123920f0e09dee382082049fefac
pedroitf6 <39974497+pedroitf6@users.noreply.github.com>
2023-02-23 17:47:02 -0300

    Incluindo comentários

--- Desafios/Java/Dominando Algoritmos Básicos com Desafios de Código Java/IndustriaDaMulta.java
+++ Desafios/Java/Dominando Algoritmos Básicos com Desafios de Código Java/IndustriaDaMulta.java
@@ -1,3 +1,16 @@
+/*Desafio
+A prefeitura de uma cidade A instalou uma câmera para verificar quem estava acima da velocidade permitida na rodovia da entrada da cidade. 
+Porém, ela é uma cidade extremamente movimentada e a prefeitura não deu conta de enviar as multas necessárias para quem ultrapassasse a 
+velocidade permitida de 60km/h.
+
+O seu papel será o de criar um programa que verifique os dados recebidos da câmera e envie uma mensagem dizendo se o motorista será multado ou não.*/
+
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:  
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next"; 
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.   
+
+
+
 import java.util.Scanner;
 
 public class IndustriaDaMulta {
