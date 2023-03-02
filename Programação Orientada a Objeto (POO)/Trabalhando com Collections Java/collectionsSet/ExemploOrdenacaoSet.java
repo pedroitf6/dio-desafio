dda51453ad9d12bdfcc63400df9803c7b8efb1bc
Pedro Tavares <Pedroitf6@gmail.com>
2023-03-02 17:56:08 -0300

    Atualização dos exemplos sobre Collections Set - Ordenação de elementos

--- Programação Orientada a Objeto (POO)/Trabalhando com Collections Java/collectionsSet/ExemploOrdenacaoSet.java
+++ Programação Orientada a Objeto (POO)/Trabalhando com Collections Java/collectionsSet/ExemploOrdenacaoSet.java
@@ -16,36 +16,48 @@ public class ExemploOrdenacaoSet {
         System.out.println("--\tOrdem aleatória\t--");
         Set<Serie> minhasSeries = new HashSet<Serie>(){{
             add(new Serie("got", "fantasia", 60));
-            add(new Serie("dark", "drama", 60));
+            add(new Serie("dark", "drama", 10));
             add(new Serie("that '70s show", "comédia", 25));
         }};
+
         for (Serie serie: minhasSeries) System.out.println(serie.getNome() + " - "
                 + serie.getGenero() + " - " + serie.getTempoEpisodio());
 
         System.out.println("--\tOrdem inserção\t--");
-        Set<Serie> minhasSeries1 = new LinkedHashSet<>() {{
+       
+        Set<Serie> minhasSeries1 = new LinkedHashSet<Serie>(){{
             add(new Serie("got", "fantasia", 60));
             add(new Serie("dark", "drama", 60));
             add(new Serie("that '70s show", "comédia", 25));
         }};
-        for (Serie serie: minhasSeries1) System.out.println(serie.getNome() + " - "
-                + serie.getGenero() + " - " + serie.getTempoEpisodio());
 
+        for (Serie serie: minhasSeries1) System.out.println(serie.getNome() + " - "
+        + serie.getGenero() + " - " + serie.getTempoEpisodio());
         System.out.println("--\tOrdem natural (TempoEpisodio)\t--");
         Set<Serie> minhasSeries2 = new TreeSet<>(minhasSeries1);
         for (Serie serie: minhasSeries2) System.out.println(serie.getNome() + " - "
-                + serie.getGenero() + " - " + serie.getTempoEpisodio());
-
+        + serie.getGenero() + " - " + serie.getTempoEpisodio());
+        
         System.out.println("--\tOrdem Nome/Gênero/TempoEpisodio\t--");
         Set<Serie> minhasSeries3 = new TreeSet<>(new ComparatorNomeGeneroTempoEpisodio());
         minhasSeries3.addAll(minhasSeries);
         for (Serie serie: minhasSeries3) System.out.println(serie.getNome() + " - "
-                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+        + serie.getGenero() + " - " + serie.getTempoEpisodio());
+       
 
 //Pra você
-/*        System.out.println("--\tOrdem gênero\t--");
-          System.out.println("--\tOrdem Tempo Episódio\t--");
- */
+        System.out.println("--\tOrdem Tempo Episódio\t--");
+        Set<Serie> minhSeries4 = new TreeSet<>();
+        minhSeries4.addAll(minhasSeries);
+        for (Serie serie: minhSeries4) 
+        System.out.println(serie.getNome() + " - "
+        + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("--\tOrdem gênero\t--");
+        Set<Serie> minhasSeries5 = new TreeSet<>(new ComparatorGenero());
+        minhasSeries5.addAll(minhasSeries);
+        for (Serie serie: minhasSeries5) System.out.println(serie.getNome() + " - "
+        + serie.getGenero() + " - " + serie.getTempoEpisodio());
 
     }
 }
@@ -111,6 +123,18 @@ class ComparatorNomeGeneroTempoEpisodio implements Comparator<Serie>{
         int nome = s1.getNome().compareTo(s2.getNome());
         if (nome != 0) return nome;
 
+        int genero = s1.getGenero().compareTo(s2.getGenero());
+        if (genero != 0) return genero;
+
+        return Integer.compare(s1.getTempoEpisodio(), s2.getTempoEpisodio());
+    }
+}
+
+class ComparatorGenero implements Comparator<Serie>{
+
+    @Override
+    public int compare(Serie s1, Serie s2) {
+        
         int genero = s1.getGenero().compareTo(s2.getGenero());
         if (genero != 0) return genero;
 
